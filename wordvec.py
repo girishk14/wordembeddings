d9dc29cf8976839e084fa489dbbc7aa21e7562f4
GK <girishk14@tamu.edu>
2016-03-25 20:26:01 -0700

    First commit

---
+++ wordvec.py
@@ -0,0 +1,49 @@
+import numpy as np
+la = np.linalg
+
+
+
+
+
+
+
+
+
+
+
+def main():
+	sentences = ["I like deep learning", "I love NLP", "I dont love my girlfriend", "I love Wonderwoman"]
+	m = sent2covec(sentences)
+
+
+
+
+
+def sent2covec(sentences):
+	word_list = sorted(set([word.lower() for sent in sentences for word in sent.split()]))
+	invmap = {}
+	for idx, word in enumerate(word_list):
+		invmap[word] = idx
+	covec = np.zeros([len(word_list), len(word_list)])
+	
+	print(word_list)
+
+	for sent in sentences:
+		words = [w.lower() for w in sent.split()]
+		for idx,word in enumerate(words):
+
+			if idx - 1 >= 0:
+				covec[invmap[word]][invmap[words[idx-1]]] +=1
+
+		
+			
+			if idx + 1 < len(words):
+				covec[invmap[word]][invmap[words[idx+1]]] +=1
+
+	
+
+
+	print(covec)
+
+if __name__ == "__main__":
+	main()
