6ea865a8a570130c29629f0ebe3a2beed183e368
GK <girishk14@tamu.edu>
2016-04-07 08:59:01 -0700

    Word embeddings!

--- wordvec.py
+++ wordvec.py
@@ -1,23 +1,21 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 la = np.linalg
-
-
-
-
-
-
-
 
 
 
 
 def main():
 	sentences = ["I like deep learning", "I love NLP", "I dont love my girlfriend", "I love Wonderwoman"]
-	m = sent2covec(sentences)
+	words, m = sent2covec(sentences)
+	print(m)
+	U, S, V_t = 	la.svd(m,full_matrices = False)
+	
+	for i in xrange(len(m)):
+		plt.text(U[i,0], U[i,1],words[i])
 
-
-
-
+	plt.show()
 
 def sent2covec(sentences):
 	word_list = sorted(set([word.lower() for sent in sentences for word in sent.split()]))
@@ -42,8 +40,6 @@ def sent2covec(sentences):
 
 	
 
-
-	print(covec)
-
+	return word_list,covec
 if __name__ == "__main__":
 	main()
